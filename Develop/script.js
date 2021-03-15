6918f97874e9ed8bb015db48cfff8cd7cd161026
jermz678 <jermz678@live.com>
2021-03-14 21:06:40 -0500

    adding initial if statements for user choices

--- Develop/script.js
+++ Develop/script.js
@@ -31,18 +31,20 @@ function generatePassword(){
         alert ( "please choose again");
          userChoiceBigLetters = prompt("Would you like uppercase letters in your password? Type Y for yes, and N for no. ");
     }
+    //small letters prompt and while loop
     var userPromptSmallLetters = prompt("Would you like lowercase letter in your password? Type Y for yes, and N for no. ");
     userChoiceSmallLetters = userPromptSmallLetters.toLowerCase();
     while ( userChoiceSmallLetters  !== "y" &&  userChoiceSmallLetters !== "n" ){
         alert ( "please choose again");
         userChoiceSmallLetters = prompt("Would you like lower letters in your password? Type Y for yes, and N for no. ");
     } 
+    //numbers prompt and while loop
     var userPromptNumbers = prompt("Would you like numbers in your password? Type Y for yes, and N for no. ");
     userChoiceNumbers = userPromptNumbers.toLowerCase();
    while ( userChoiceNumbers !== "y"  && userChoiceNumbers !== "n" ){
         alert ( "please choose again");
         userChoiceNumbers = prompt("Would you like numbers in your password? Type Y for yes, and N for no. ");
-    }
+    }//symbols prompt and while loop
     var userPromptSymbols = prompt("would you like symbols in your password?Type Y for yes, and N for no. ");
     userChoiceSymbols = userPromptSymbols.toLowerCase();
     while ( userChoiceSymbols != "y" && userChoiceSymbols != "n" ){
@@ -52,15 +54,48 @@ function generatePassword(){
     if(userChoiceSymbols === "n"  && userChoiceNumbers === "n" && userChoiceSmallLetters === "n" && userChoiceBigLetters === "n"){
         alert("sorry, we can not make you a password.  Please reload page and try again")
     }
+
+     if((userChoiceSymbols === "n"  && userChoiceNumbers === "n" && userChoiceSmallLetters === "n" && userChoiceBigLetters === "y")){
+      for (i = 0; i < userChoiceLength; i++){
+        ar.push(bigLetters[Math.floor(Math.random() * bigLetters.length)]); 
+      }
+    }
+    
+     if ((userChoiceSymbols === "n"  && userChoiceNumbers === "n" && userChoiceSmallLetters === "y" && userChoiceBigLetters === "n")){
+        for (i = 0; i < userChoiceLength; i++){
+          ar.push(smallLetters[Math.floor(Math.random() * smallLetters.length)]); 
+        }
+      }
+
+      if ((userChoiceSymbols === "n"  && userChoiceNumbers === "y" && userChoiceSmallLetters === "n" && userChoiceBigLetters === "n")){
+        for (i = 0; i < userChoiceLength; i++){
+          ar.push(numbers[Math.floor(Math.random() * numbers.length)]); 
+        }
+      }
+      
+      if ((userChoiceSymbols === "y"  && userChoiceNumbers === "n" && userChoiceSmallLetters === "n" && userChoiceBigLetters === "n")){
+        for (i = 0; i < userChoiceLength; i++){
+          ar.push(symbols[Math.floor(Math.random() * symbols.length)]); 
+        }
+      }
+  
+  
+
+    
     
 
+userChoiceSmallLetters
+userChoiceBigLetters
+userChoiceSymbols
+userChoiceNumbers
 
     var newArray = symbols.concat(numbers);
-console.log(newArray)
+    console.log(newArray)
+
     // getting userchoice to fill empty array
     
       // taking commas out of ar array
-      console.log(ar);
+      
     return ar.join("");
     
 };
